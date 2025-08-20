60cadf5b4ab9251dd6a568ab006272ae198e76c4
funkpopo <funkpopoisme@gmail.com>
2025-08-20 10:15:12 +0800

    移除网络状态管理器及相关组件，简化代码结构，提升应用性能和可维护性。同时，删除网络状态指示器组件，减少不必要的依赖。

--- src/app.jsx
+++ src/app.jsx
@@ -77,7 +77,6 @@ import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import AddIcon from "@mui/icons-material/Add";
 import { dispatchCommandToGroup } from "./core/syncGroupCommandDispatcher";
 import { useEventManager } from "./core/utils/eventManager.js";
-import NetworkStatusIndicator from "./components/NetworkStatusIndicator.jsx";
 
 // 自定义磨砂玻璃效果的Dialog组件
 const GlassDialog = styled(Dialog)(({ theme }) => ({
@@ -1829,10 +1828,6 @@ function App() {
               })}
             </Tabs>
 
-            {/* 网络状态指示器 */}
-            <Box sx={{ ml: 2, display: 'flex', alignItems: 'center' }}>
-              <NetworkStatusIndicator />
-            </Box>
 
             {/* 标签页右键菜单 */}
             <Menu
