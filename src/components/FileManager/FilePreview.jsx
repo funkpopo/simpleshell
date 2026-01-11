e3c59c469be49567ef0ce0eb4f2cf09c1d76f405
funkpopo <s767609509@gmail.com>
2026-01-11 12:01:33 +0800

    删除FileManager组件及其相关子组件，简化项目结构，提升可维护性。

--- src/components/FileManager/FilePreview.jsx
+++
@@ -1,6 +0,0 @@
-// FilePreview组件包装器
-// 导出现有的FilePreview组件，保持向后兼容
-export { default } from "../FilePreview.jsx";
-
-// 也可以从这里导入使用
-export { default as FilePreviewComponent } from "../FilePreview.jsx";
